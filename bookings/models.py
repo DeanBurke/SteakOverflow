80994f16ef431b4ad29586255eeb344f148064f9
DeanBurke <deanburke07@hotmail.com>
2023-07-19 18:50:00 +0000

    Added booking model to bookings app

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+BOOKING_STATUS = ((0, "Pending"), (1, "Approved"), (2, "Denied"))
+
+class Booking(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, default=User)
+    reference = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField(default=(19, 30))
+    group_size = models.IntegerField(
+        default=1,
+        validators=[
+            MaxValueValidator(100),
+            MinValueValidator(1)
+        ]
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=BOOKING_STATUS, default=0)
+
+    class Meta:
+        ordering = ['-date']
