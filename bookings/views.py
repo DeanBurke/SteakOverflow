71f89183c05a487635fd59e4dc1726cc668aa594
DeanBurke <deanburke07@hotmail.com>
2023-07-20 18:05:33 +0000

    Added template from Bootstrapmade as a basis for initial bootstrap

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
-# Create your views here.
+
+class BookingsApproved(generic.ListView):
+    model = Booking
+    queryset = Booking.objects.filter(status=1).order_by('date')
+    template_name = 'bookings.html'
