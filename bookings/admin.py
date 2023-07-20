b605783d0fa3d0f7e193c65750c6a90b6ef9c167
DeanBurke <deanburke07@hotmail.com>
2023-07-20 17:08:26 +0000

    Added filter, display and search fields to admin page for bookings

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(ModelAdmin):
+    list_display = ('customer', 'reference', 'date', 'time', 'group_size', 'status')
+    search_fields = ('customer', 'reference''date', 'group_size', 'status')
+    list_filter = ('date', 'status')
