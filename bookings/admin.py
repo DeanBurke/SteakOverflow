f09159e96a3346c45d3da6e34227b5a96c7caad4
DeanBurke <deanburke07@hotmail.com>
2023-07-20 17:19:23 +0000

    Added actions along with relevant functions to BookingAdmin class

--- bookings/admin.py
+++ bookings/admin.py
@@ -7,3 +7,13 @@ class BookingAdmin(ModelAdmin):
     list_display = ('customer', 'reference', 'date', 'time', 'group_size', 'status')
     search_fields = ('customer', 'reference''date', 'group_size', 'status')
     list_filter = ('date', 'status')
+    actions = ['pending_booking', 'approve_booking', 'deny_booking']
+
+    def pending_booking(self, request, queryset):
+        queryset.update(status=0)
+
+    def approve_booking(self, request, queryset):
+        queryset.update(status=1)
+
+    def deny_booking(self, request, queryset):
+        queryset.update(status=2)
