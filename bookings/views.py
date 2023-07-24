20cf3f1c4feb900e3b09dc43ba5174352f64d1a9
DeanBurke <deanburke07@hotmail.com>
2023-07-24 10:58:44 +0000

    Adding bookings view and styling to html page

--- bookings/views.py
+++ bookings/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Booking
+from .forms import BookingForm
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
 
 
-class BookingsApproved(generic.ListView):
+class BookingsApproved(LoginRequiredMixin, generic.ListView):
     model = Booking
     queryset = Booking.objects.filter(status=1).order_by('date')
     template_name = 'bookings.html'
--- steakoverflow/settings.py
+++ steakoverflow/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'bookings',
 ]
 
@@ -58,6 +59,8 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISP_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
